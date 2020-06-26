9839bd99fc0b4eed8ff5b195d1a19eabfa12431d
Hamad-158 <Hamad.bfar@gmail.com>
2020-06-26 07:22:00 +0300

    هالمره صج اخر مره

--- hw4/app/src/main/java/com/example/hw4/thecal.java
+++ hw4/app/src/main/java/com/example/hw4/thecal.java
@@ -2,7 +2,6 @@ package com.example.hw4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -14,11 +13,13 @@ public class thecal<b, Beeez> extends AppCompatActivity {
     float a, b, c;
     String scrtxt, sin, sint, calr;
     TextView textView;
+    TextView textView0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thecal);
+        textView0 = findViewById(R.id.textView0);
         textView = (TextView) findViewById(R.id.textView3);
         sint = "0";
         scrtxt = "";
@@ -80,16 +81,49 @@ public class thecal<b, Beeez> extends AppCompatActivity {
     public void color() {
         if (Double.parseDouble(calr) >= 90 && Double.parseDouble(calr) <= 100) {
             textView.setTextColor(Color.GREEN);
+            textView0.setText(Gredso());
         } else if (Double.parseDouble(calr) >= 80 && Double.parseDouble(calr) < 90) {
             textView.setTextColor(Color.BLUE);
+            textView0.setText(Greds());
         } else if (Double.parseDouble(calr) >= 70 && Double.parseDouble(calr) < 90) {
             textView.setTextColor(Color.YELLOW);
+            textView0.setText(Greds2());
         } else if (Double.parseDouble(calr) >= 60 && Double.parseDouble(calr) < 70) {
             textView.setTextColor(Color.RED);
+            textView0.setText(Greds3());
         }else if (Double.parseDouble(calr) >= 1 && Double.parseDouble(calr) < 60) {
             textView.setTextColor(Color.DKGRAY);
+            textView0.setText(Greds4());
         }else  {
             textView.setTextColor(Color.WHITE);
+            textView0.setText(Greds5());
+
         }
     }
+
+    private String Greds5() {
+        return "NO Cooment";
+    }
+
+    private String Greds4() {
+        return "F";
+    }
+
+    private String Greds3() {
+        return "D";
+    }
+
+    private String Gredso() {
+        return "A";
+    }
+
+    private String Greds2() {
+        return "C";
+    }
+
+    private String Greds() {
+        return "B";
+    }
+
+
 }
