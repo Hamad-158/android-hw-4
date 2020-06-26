451225229caf6298a9a2042779c7c4066ab202f8
Hamad-158 <Hamad.bfar@gmail.com>
2020-06-26 03:47:13 +0300

    اخر تعديل

--- hw4/app/src/main/java/com/example/hw4/thecal.java
+++ hw4/app/src/main/java/com/example/hw4/thecal.java
@@ -2,6 +2,8 @@ package com.example.hw4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,29 +11,29 @@ import android.widget.TextView;
 
 public class thecal<b, Beeez> extends AppCompatActivity {
 
-    float a , b , c;
-    String scrtxt , sin , sint , calr;
-    TextView textView ;
+    float a, b, c;
+    String scrtxt, sin, sint, calr;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thecal);
-        textView =(TextView)findViewById(R.id.textView3);
+        textView = (TextView) findViewById(R.id.textView3);
         sint = "0";
         scrtxt = "";
         calr = "0";
     }
 
     public void onClicknumber(View view) {
-        calr="0";
-        if (sint.equals("0")){
-            Button button = (Button)view;
+        calr = "0";
+        if (sint.equals("0")) {
+            Button button = (Button) view;
             scrtxt += button.getText().toString();
             textView.setText(scrtxt);
             a = Float.parseFloat(scrtxt);
-        }else {
-            Button button = (Button)view;
+        } else {
+            Button button = (Button) view;
             scrtxt += button.getText().toString();
             textView.setText(scrtxt);
             b = Float.parseFloat(scrtxt);
@@ -40,37 +42,54 @@ public class thecal<b, Beeez> extends AppCompatActivity {
     }
 
     public void onClickss(View view) {
-        Button button = (Button)view;
+        Button button = (Button) view;
         sin = button.getText().toString();
         textView.setText(sin);
-        sint ="1";
-        scrtxt="";
+        sint = "1";
+        scrtxt = "";
 
     }
 
     public void onClickE(View view) {
-        Button button = (Button)view;
+        Button button = (Button) view;
 
-        if (sin.equals("+")){
-            c = a+b;
-        }else if (sin.equals("-")){
-            c = a-b;
-        }else if (sin.equals("*")){
+        if (sin.equals("+")) {
+            c = a + b;
+        } else if (sin.equals("-")) {
+            c = a - b;
+        } else if (sin.equals("*")) {
             c = a * b;
-        }else if (sin.equals("/")){
+        } else if (sin.equals("/")) {
             c = a / b;
         }
         calr = Float.toString(c);
         textView.setText(calr);
+        color();
         scrtxt = "0";
-        sint="0";
+        sint = "0";
     }
 
     public void rest(View view) {
-        Button button = (Button)view;
+        Button button = (Button) view;
         sint = "0";
         scrtxt = "";
         calr = "0";
         textView.setText("0");
+    }
+
+    public void color() {
+        if (Double.parseDouble(calr) >= 90 && Double.parseDouble(calr) <= 100) {
+            textView.setTextColor(Color.GREEN);
+        } else if (Double.parseDouble(calr) >= 80 && Double.parseDouble(calr) < 90) {
+            textView.setTextColor(Color.BLUE);
+        } else if (Double.parseDouble(calr) >= 70 && Double.parseDouble(calr) < 90) {
+            textView.setTextColor(Color.YELLOW);
+        } else if (Double.parseDouble(calr) >= 60 && Double.parseDouble(calr) < 70) {
+            textView.setTextColor(Color.RED);
+        }else if (Double.parseDouble(calr) >= 1 && Double.parseDouble(calr) < 60) {
+            textView.setTextColor(Color.DKGRAY);
+        }else  {
+            textView.setTextColor(Color.WHITE);
+        }
     }
 }
